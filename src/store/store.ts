e3b4e41d35fcf0793acd9fbcf69edb378fb469c4
FelixShih <zx8714245@gmail.com>
2024-02-03 20:10:08 +0800

    feat: modify project structre

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
+import whiteboardSlice from "./whiteboard";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    whiteboard: whiteboardSlice.reducer,
   },
 });
 
