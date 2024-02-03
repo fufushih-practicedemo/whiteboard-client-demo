2db97da25450b6d9fa3cc10da71fc854d95144d9
FelixShih <zx8714245@gmail.com>
2024-02-03 20:51:35 +0800

    chore: setting the linter and formatter in project

--- src/store/whiteboard.ts
+++ src/store/whiteboard.ts
@@ -1,17 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    tool: null
-}
+	tool: null,
+};
 
 const whiteboardSlice = createSlice({
-    name: "whiteboard",
-    initialState,
-    reducers: {
-        setToolType: (state, action) => {
-            state.tool = action.payload;
-        }
-    }
+	name: "whiteboard",
+	initialState,
+	reducers: {
+		setToolType: (state, action) => {
+			state.tool = action.payload;
+		},
+	},
 });
 
 export const { setToolType } = whiteboardSlice.actions;
